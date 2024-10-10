aac0a8a40af69f146aaa42486f275c1aaae8c766
BernardoSiqueira1 <bernardoasiqueira@hotmail.com>
2024-10-10 12:55:53 -0300

    fix: anotação de validação jakarta adicionada.

--- src/main/java/com/lojaAPP/applicationAPI/Modules/Usuarios/Controller/UsuarioController.java
+++ src/main/java/com/lojaAPP/applicationAPI/Modules/Usuarios/Controller/UsuarioController.java
@@ -3,6 +3,7 @@ package com.lojaAPP.applicationAPI.Modules.Usuarios.Controller;
 import com.lojaAPP.applicationAPI.Modules.Usuarios.DTO.NovoEnderecoDTO;
 import com.lojaAPP.applicationAPI.Modules.Usuarios.DTO.NovoUsuarioDTO;
 import com.lojaAPP.applicationAPI.Modules.Usuarios.Services.UsuarioServices;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,7 @@ public class UsuarioController implements UsuarioControllerDoc {
     private UsuarioServices usuarioServices;
 
     @PostMapping(value = "/cadastrar")
-    public ResponseEntity<?> cadastrarUsuario(@RequestBody  NovoUsuarioDTO novoUsuarioDTO){
+    public ResponseEntity<?> cadastrarUsuario(@RequestBody @Valid NovoUsuarioDTO novoUsuarioDTO){
         usuarioServices.cadastrarCliente(novoUsuarioDTO);
         return ResponseEntity.status(201).body("Usuário foi criado com sucesso.");
     }
@@ -32,17 +33,5 @@ public class UsuarioController implements UsuarioControllerDoc {
         usuarioServices.excluirUsuario(id);
 
         return ResponseEntity.status(200).body("Usuário foi excluído.");
-    }
-
-    @PostMapping(value = "/cadastrarEndereco/{id}")
-    public ResponseEntity<?> cadastrarEndereco(@PathVariable long id, @RequestBody NovoEnderecoDTO novoEnderecoDTO){
-        usuarioServices.cadastrarNovoEndereco(novoEnderecoDTO, id);
-        return ResponseEntity.status(201).body("Novo endereço cadastrado com sucesso.");
-    }
-
-    @DeleteMapping(value = "/deleteEndereco/{ìd}")
-    public ResponseEntity<?> deletarEndereco(@PathVariable long id){
-        usuarioServices.excluirUsuario(id);
-        return ResponseEntity.status(200).body("Endereço foi excluído.");
     }
 }
