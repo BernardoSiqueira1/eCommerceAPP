5126c495d05ae8f64d2881ad12041936229f5736
BernardoSiqueira1 <bernardoasiqueira@hotmail.com>
2024-10-10 12:50:53 -0300

    feat: Exception Handlers para erros de serviço

--- src/main/java/com/lojaAPP/applicationAPI/Config/ExceptionHandlerConfig.java
+++ src/main/java/com/lojaAPP/applicationAPI/Config/ExceptionHandlerConfig.java
@@ -1,10 +1,30 @@
 package com.lojaAPP.applicationAPI.Config;
 
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.PersistenceException;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 @Configuration
 public class ExceptionHandlerConfig {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> handleEntityNotFound(){
+        return ResponseEntity.status(404).body("Valor não consta na base de dados.");
+    }
+
+    @ExceptionHandler(PersistenceException.class)
+    public ResponseEntity<?> handlePersistenceErrors(){
+        return ResponseEntity.status(500).body("Não foi possível registrar a entidade, verifique os campos fornecidos.");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationErrors(){
+        return ResponseEntity.status(400).body("Não foi possível realizar o registro, verifique os campos fornecidos.");
+    }
 
 }
