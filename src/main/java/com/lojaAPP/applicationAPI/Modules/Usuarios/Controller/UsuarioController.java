3fc50658ebeb16cea28d8f84fddc3089eed5c78f
BernardoSiqueira1 <bernardoasiqueira@hotmail.com>
2024-10-15 15:06:18 -0300

    refactor: remoção da interface para documentação.

--- src/main/java/com/lojaAPP/applicationAPI/Modules/Usuarios/Controller/UsuarioController.java
+++ src/main/java/com/lojaAPP/applicationAPI/Modules/Usuarios/Controller/UsuarioController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/usuario")
-public class UsuarioController implements UsuarioControllerDoc {
+public class UsuarioController {
 
     @Autowired
     private UsuarioServices usuarioServices;
