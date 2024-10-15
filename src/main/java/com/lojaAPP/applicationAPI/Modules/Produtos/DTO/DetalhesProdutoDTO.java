b82b7f9881ad588c7e659548d2c2dd02b86f08bc
BernardoSiqueira1 <bernardoasiqueira@hotmail.com>
2024-10-15 15:04:24 -0300

    feat: adição dos DTOS de categoria e produto

--- src/main/java/com/lojaAPP/applicationAPI/Modules/Produtos/DTO/DetalhesProdutoDTO.java
+++ src/main/java/com/lojaAPP/applicationAPI/Modules/Produtos/DTO/DetalhesProdutoDTO.java
@@ -9,6 +9,7 @@ public record DetalhesProdutoDTO(
         float preco_original,
         float preco_a_vista, String descricao,
         String foto_url,
-        Categoria categoria
+        long categoriaId,
+        String nomeCategoria
 )
 {}
